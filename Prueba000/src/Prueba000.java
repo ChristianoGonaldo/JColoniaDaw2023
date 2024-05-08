a1f2fdd75e1c44613a5812351fca5fb97515f934
Daniel Díaz <daniel.diacab.1@educa.jcyl.es>
2024-05-08 09:21:30 +0200

    Addicion de prueba, para el ejemplo inutil pr

---
+++ Prueba000/src/Prueba000.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ * Crear la clase
+ */
+public class Prueba000 {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int elemento = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingresa los números a estudiar: ");
+		String Lista = scanner.nextLine();
+		Lista.add(elemento);
+		
+		System.out.println("Hola, " + elemento + "¡-");
+	}
+	
+}
